cb9586d18d675b39153dc6c0d65cad423c58e140
Lekiios <lorenzo.lacz@gmail.com>
2024-11-19 16:11:03 +0100

    Setup backend with Fastify.js and Prisma using SQLite

---
+++ backend/src/routes/example.ts
@@ -0,0 +1,61 @@
+import { server } from '../providers/server';
+import {
+  errorResponseSchema,
+  exampleQuerySchema,
+  exampleResponse,
+  exampleResponseSchema,
+} from '../schemas/schemas';
+import { db } from '../providers/db';
+
+server.get(
+  '/example',
+  {
+    schema: {
+      querystring: exampleQuerySchema,
+      response: {
+        200: exampleResponseSchema,
+        400: errorResponseSchema,
+      },
+      tags: ['Example'],
+    },
+  },
+  async (request, reply): Promise<exampleResponse> => {
+    const { id, fun } = request.query;
+
+    if (!id) {
+      return reply.status(400).send({ error: 'id is required' });
+    }
+
+    return reply.status(200).send({ toto: `Hello ${id}${fun ? ` ${fun}` : ''}` });
+  },
+);
+
+server.post(
+  '/example',
+  {
+    schema: {
+      querystring: exampleQuerySchema,
+      response: {
+        200: exampleResponseSchema,
+        400: errorResponseSchema,
+      },
+      tags: ['Example'],
+    },
+  },
+  async (request, reply): Promise<exampleResponse> => {
+    const { id, fun } = request.query;
+
+    if (!id) {
+      return reply.status(400).send({ error: 'id is required' });
+    }
+
+    db.user.create({
+      data: {
+        token: id,
+        pseudo: fun ?? id,
+      },
+    });
+
+    return reply.status(200).send({ toto: `Hello ${id}${fun ? ` ${fun}` : ''}` });
+  },
+);
