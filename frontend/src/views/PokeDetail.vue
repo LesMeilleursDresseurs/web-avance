2e8c188f4001d8d545ba164b34db93f9fd65e49d
Lekiios <lorenzo.lacz@gmail.com>
2024-11-21 09:45:25 +0100

    Lazy loading list for cards of a pokemon

--- frontend/src/views/PokeDetail.vue
+++ frontend/src/views/PokeDetail.vue
@@ -1,14 +1,36 @@
 <template>
   <main>
-    <div class="grid">
-      <article v-for="card in cards" :key="card.id" class="card" @click="logCard(card)">
-        <img :src="`${card.image}/low.webp`" :alt="card.name" class="card-img" />
-      </article>
+    <MenuTopBar />
+    <h1>{{ name }}</h1>
+    <div class="gridContainer">
+      <div class="grid">
+        <div
+          class="cardBox"
+          v-for="card in cards"
+          v-bind:key="card.id"
+          @click="console.log(card.id, card.image)"
+        >
+          <img
+            v-if="card.image"
+            :src="`${card.image}/low.webp`"
+            :alt="card.name"
+            class="cardImg"
+            :class="{ bigImg: hover === card.id }"
+          />
+          <div v-else>
+            <h4>{{ card.name }}</h4>
+            <h4>{{ card.id }}</h4>
+          </div>
+        </div>
+      </div>
     </div>
+    <div v-if="isLoading" class="loading"><img src="@/assets/loader.gif" alt="loader" /></div>
   </main>
 </template>
 
 <script lang="ts">
+import MenuTopBar from '@/components/MenuTopBar.vue'
+
 type CardBrief = {
   id: string
   localId: string
@@ -17,13 +39,23 @@ type CardBrief = {
 }
 
 interface IState {
-  cards?: Array<CardBrief>
+  cards: Array<CardBrief>
+  isLoading: boolean
+  page: number
+  hover: string
+  timeout?: number
 }
 
+const itemPerPage = 15
+
 export default {
+  components: { MenuTopBar },
   data(): IState {
     return {
       cards: [],
+      isLoading: false,
+      page: 1,
+      hover: '',
     }
   },
   props: {
@@ -34,100 +66,92 @@ export default {
   },
   methods: {
     async fetchCards() {
-      const res = await fetch(`https://api.tcgdex.net/v2/en/cards?name=${this.name}`)
-      this.cards = await res.json()
-    },
-    logCard(card: CardBrief) {
-      console.log(card.id, card.localId, card.image, card.name)
+      this.isLoading = true
+      const res = await fetch(
+        `https://api.tcgdex.net/v2/en/cards?name=${this.name}&category=Pokemon&pagination:page=${this.page}&pagination:itemsPerPage=${itemPerPage}`,
+      )
+      const page = await res.json()
+      if (page.length === 0) {
+        this.isLoading = false
+        window.removeEventListener('scroll', () => {})
+        return
+      }
+      this.cards = [...this.cards, ...page]
+      this.page++
+      setTimeout(() => {
+        this.isLoading = false
+      }, 1000)
     },
   },
   created() {
-    this.$watch(() => this.$route.params.id, this.fetchCards, { immediate: true })
+    this.$watch(() => this.$route.params.name, this.fetchCards, { immediate: true })
+
+    window.addEventListener('scroll', async () => {
+      if (
+        window.innerHeight + window.scrollY >= document.body.offsetHeight - 100 &&
+        !this.isLoading
+      ) {
+        await this.fetchCards()
+        console.log('event')
+      }
+    })
+  },
+  beforeUnmount() {
+    window.removeEventListener('scroll', () => {})
   },
 }
 </script>
 
 <style scoped>
-.loading {
-  width: 100%;
-  display: flex;
-  justify-content: center;
-}
-.loading img {
-  width: 6vw;
-}
-
-h1 {
-  text-align: center;
-  margin: 1rem auto;
-  font-size: 1.8vw;
-}
-
-header {
-  margin-bottom: 0;
-}
-
 .grid {
   display: flex;
   flex-wrap: wrap;
   justify-content: center;
   gap: 2rem;
-  margin: 1rem 3vw;
-  padding: 0 1vw;
 }
 
-.card {
-  flex: 0 1 calc(20% - 2rem);
-  box-sizing: content-box;
-  padding: 1rem;
-  border-radius: 0.5rem;
-  text-align: center;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  margin-bottom: 0;
-  transition: transform 0.2s;
-  position: relative;
-}
-
-div.idPoke {
-  position: absolute;
-  top: 1vw;
-  right: 1.2vw;
-  font-size: 1vw;
-  background-color: #6b6d65;
-  border: #f2f2f2 solid 1px;
-  border-radius: 0.5rem;
-  padding: 3px;
-}
-
-.card:hover {
-  transform: scale(1.2);
-}
-
-.card footer h4 {
-  font-size: 1.4vw;
-  margin-bottom: 0;
-}
-
-img.card-img {
-  max-width: 100%;
-  width: 10vw;
-  image-rendering: pixelated;
-}
-
-button.secondary {
-  display: block;
-  margin: 2rem auto;
-  padding: 0.75rem 2rem;
-  background-color: #007bff;
-  color: white;
-  border: none;
-  border-radius: 0.5rem;
+.cardBox {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 250px;
+  border-radius: 10px;
   cursor: pointer;
-  font-size: 1rem;
-  transition: background-color 0.2s;
+  gap: 1rem;
+  padding: 10px;
+  background-color: #424242;
+  transition: transform 0.2s;
 }
 
-button.secondary:hover {
-  background-color: #0056b3;
+.cardImg {
+  width: 100%;
+  height: 100%;
+  box-shadow: 0 0 10px 0 rgba(0, 0, 0, 0.1);
+}
+
+.cardBox:hover {
+  transform: scale(1.15);
+}
+
+h1 {
+  text-align: center;
+  margin: 1rem auto;
+  font-size: 3rem;
+  text-transform: capitalize;
+}
+
+h4 {
+  text-align: center;
+}
+
+.loading {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+}
+
+.loading img {
+  width: 6vw;
 }
 </style>
