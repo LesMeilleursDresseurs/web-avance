f316518fd92e058176d19d6984fe308c5f4e1784
Camille Berquet <camille.berquet@united-vr.com>
2024-11-21 16:59:58 +0100

    fix the responsive of the modal

--- frontend/src/components/CardModal.vue
+++ frontend/src/components/CardModal.vue
@@ -1,85 +1,81 @@
 <template>
-  <div class="background">
-    <div class="modal-backdrop">
-      <div class="modal">
-        <header class="modal-header">
-          <button type="button" class="btn-close" @click="close">x</button>
-        </header>
+  <div class="modal-backdrop">
+    <div class="modal">
+      <header class="modal-header">
+        <button type="button" class="btn-close" @click="close">x</button>
+      </header>
 
-        <section class="modal-body">
-          <div class="left-section">
-            <img :src="card.image + `/high.png`" alt="Card Image" />
+      <section class="modal-body">
+        <div class="left-section">
+          <img :src="card.image + `/high.png`" alt="Card Image" />
+        </div>
+        <div class="right-section">
+          <h1>{{ card.name }} #{{ initialPokemon.id }}</h1>
+          <div class="card-info">
+            <span class="badge-default">HP : {{ card.hp }}</span>
+            <span class="badge-default">Rarity : {{ card.rarity }}</span>
           </div>
-          <div class="right-section">
-            <h1>{{ card.name }} #{{ card.id }}</h1>
-            <div class="card-info">
-              <span class="badge-default">HP : {{ card.hp }}</span>
-              <span class="badge-default">Rarity : {{ card.rarity }}</span>
-            </div>
-            <div class="card-type">
-              <h2>Type</h2>
-              <span v-for="type in card.types" :key="type" :class="getBadgeClass(type)">
-                {{ type }}
-              </span>
-            </div>
+          <div class="card-type">
+            <h2>Type</h2>
+            <span v-for="type in card.types" :key="type" :style="getBadgeTypeStyle(type)">
+              {{ type }}
+            </span>
+          </div>
 
-            <div class="card-weakness">
-              <h2>Weaknesses</h2>
-              <span
-                v-for="(weakness, index) in card.weaknesses"
-                :key="weakness.type"
-                :class="getBadgeClass(weakness.type)"
-              >
-                {{ weakness.type }}
-              </span>
-            </div>
-            <div class="card-description">
-              <h2>Story</h2>
-              <p>{{ card.description }}</p>
-            </div>
-            <div class="card-version">
-              <h2>Versions</h2>
-              <div class="card-variants">
-                <span v-for="(value, key) in card.variants">
-                  <span v-if="value === true" :class="getVariantClass(key)">
-                    <span :class="{ 'badge-variant': key === 'holo' }">{{ key }}</span>
-                  </span>
+          <div class="card-weakness">
+            <h2>Weaknesses</h2>
+            <span
+              v-for="(weakness, index) in card.weaknesses"
+              :key="weakness.type"
+              :style="getBadgeTypeStyle(weakness.type.toLowerCase())"
+            >
+              {{ weakness.type }}
+            </span>
+          </div>
+          <div class="card-description">
+            <h2>Story</h2>
+            <p>{{ card.description }}</p>
+          </div>
+          <div class="card-version">
+            <h2>Versions</h2>
+            <div class="card-variants">
+              <span v-for="(value, key) in card.variants">
+                <span v-if="value === true" :style="getBadgeVariantStyle(key)">
+                  <span :class="{ 'badge-child-holo': key === 'holo' }">{{ key }}</span>
                 </span>
-              </div>
-            </div>
-            <div v-if="card.evolveFrom">
-              <h2>Evolve From</h2>
-              <span class="card-evolvefrom">
-                {{ card.evolveFrom }}
-                <img
-                  :src="evolveFromPokemon.image"
-                  :alt="evolveFromPokemon.name"
-                  class="card-img"
-                />
               </span>
-              <span class="card-evolvefrom">
+            </div>
+          </div>
+          <div v-if="card.evolveFrom" class="card-evolvefrom">
+            <h2>Evolve From</h2>
+            <div class="evolution-pokemon">
+              <section class="child-pokemon-img">
+                {{ card.evolveFrom }}
+                <img :src="childPokemon.image" :alt="childPokemon.name" class="card-img" />
+              </section>
+              <section class="pokemon-initial-img">
                 {{ card.name }}
                 <img :src="initialPokemon.image" :alt="initialPokemon.name" class="card-img" />
-              </span>
+              </section>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
 
-        <footer class="modal-footer">
-          <slot name="footer"> This is the default footer! </slot>
-          <button type="button" class="btn-green" @click="close">Close Modal</button>
-        </footer>
-      </div>
+      <footer class="modal-footer">
+        <slot name="footer"> This is the default footer! </slot>
+        <button type="button" class="btn-green" @click="close">Close Modal</button>
+      </footer>
     </div>
   </div>
 </template>
-
 <script>
+import { colors } from '@/constants/constants'
 export default {
   data() {
     return {
       initialPokemon: Object,
-      evolveFromPokemon: Object,
+      childPokemon: Object,
     }
   },
   name: 'Modal',
@@ -89,6 +85,7 @@ export default {
   watch: {
     card: {
       handler(card) {
+        console.log(card)
         if (card && card.name) {
           this.fetchinitialPokemon(card.name)
         }
@@ -103,49 +100,34 @@ export default {
     close() {
       this.$emit('close')
     },
-    getBadgeClass(type) {
-      switch (type) {
-        case 'Fire':
-          return 'badge-fire'
-        case 'Water':
-          return 'badge-water'
-        case 'Grass':
-          return 'badge-grass'
-        case 'Electric':
-          return 'badge-electric'
-        case 'Psychic':
-          return 'badge-psychic'
-        case 'Darkness':
-          return 'badge-darkness'
-        case 'Fighting':
-          return 'badge-fighting'
-        case 'Metal':
-          return 'badge-metal'
-        case 'Dragon':
-          return 'badge-dragon'
-        case 'Fairy':
-          return 'badge-fairy'
-        case 'Colorless':
-          return 'badge-colorless'
-        default:
-          return 'badge-default'
+    getBadgeTypeStyle(type) {
+      const color = colors.type[type.toLowerCase()]
+      return {
+        backgroundColor: color,
+        color: 'white',
+        padding: '0.5vh 0.5vh',
+        borderRadius: '20px',
+        margin: '0 0.5vw',
       }
     },
-    getVariantClass(variant_key) {
-      console.log(variant_key)
-      switch (variant_key) {
-        case 'holo':
-          return 'badge-holo'
-        case 'reverse':
-          return 'badge-reverse'
-        case 'firstEdition':
-          return 'badge-firstEdition'
-        case 'normal':
-          return 'badge-normal'
-        case 'wPromo':
-          return 'badge-wPromo'
-        default:
-          return 'badge-default'
+    getBadgeVariantStyle(variant) {
+      if (variant !== 'holo') {
+        const color = colors.variant[variant]
+        return {
+          padding: '0.5vh 0.5vh',
+          borderRadius: '20px',
+          margin: '0 0.5vw',
+          border: `2px solid ${color}`,
+          backgroundColor: 'transparent',
+        }
+      }
+      return {
+        padding: '0.5vh 0vw',
+        border: '3px solid transparent',
+        borderRadius: '20px',
+        margin: '0 0.5vw',
+        background:
+          'linear-gradient(90deg, rgba(255, 69, 69, 1) 0%, rgba(251, 163, 72, 1) 12%, rgba(242, 244, 81, 1) 23%, rgba(181, 255, 84, 1) 33%, rgba(118, 243, 186, 1) 46%, rgba(99, 184, 248, 1) 56%, rgba(153, 150, 249, 1) 70%, rgba(252, 169, 248, 1) 84%, rgba(255, 125, 152, 1) 93%, rgba(255, 69, 69, 1) 100%)',
       }
     },
     async fetchinitialPokemon(pokemonName) {
@@ -169,7 +151,7 @@ export default {
           `https://pokeapi.co/api/v2/pokemon/${evolveFrom.toLowerCase()}`,
         )
         const pokemon = await response.json()
-        this.evolveFromPokemon = {
+        this.childPokemon = {
           id: pokemon.id,
           name: pokemon.name,
           image: pokemon.sprites.front_default || '',
@@ -183,11 +165,6 @@ export default {
 }
 </script>
 <style>
-.background {
-  position: absolute;
-  height: 100vh;
-  width: 100vw;
-}
 .modal-backdrop {
   position: fixed;
   top: 0;
@@ -198,26 +175,26 @@ export default {
   display: flex;
   justify-content: center;
   align-items: center;
+  margin:;
 }
 
 .modal {
   background: #ffffff;
-  box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-  border-radius: 9px;
-  width: 90vw;
+  box-shadow: 2px 2px 20px 1px;
   overflow-x: auto;
   display: flex;
   flex-direction: column;
+  margin: 2vh 2vw;
 }
 
 .modal-header,
 .modal-footer {
-  padding: 15px;
+  padding: 1vh 2vw;
   display: flex;
 }
 
 .modal-header {
-  display: flex;
+  position: relative;
   border-bottom: 1px solid #eeeeee;
   color: #4aae9b;
   justify-content: space-between;
@@ -232,9 +209,8 @@ export default {
 .modal-body {
   display: flex;
   flex-direction: row;
-  padding: 20px 10px;
+  padding: 20px 0;
 }
-
 .left-section,
 .right-section {
   display: flex;
@@ -243,7 +219,7 @@ export default {
 
 .left-section {
   border-right: 1px solid #ddd;
-  padding: 3vw;
+  padding: 0 1vw;
 }
 
 .left-section img {
@@ -257,119 +233,14 @@ export default {
   margin: 0 3vw;
 }
 .right-section h1 {
-  font-size: 3em;
+  font-size: 2em;
   font-weight: bold;
   margin-bottom: 1rem;
 }
 .right-section h2 {
-  font-size: 1.5em;
+  font-size: 1em;
   font-weight: bold;
   margin-bottom: 1vh;
-}
-.badge-fire,
-.badge-water,
-.badge-grass,
-.badge-electric,
-.badge-psychic,
-.badge-darkness,
-.badge-fighting,
-.badge-metal,
-.badge-dragon,
-.badge-fairy,
-.badge-colorless,
-.badge-normal,
-.badge-reverse,
-.badge-wPromo,
-.badge-firstEdition,
-.badge-default {
-  padding: 0.5vh 0.5vh;
-  background-color: #d2d1c1;
-  border: 3px solid transparent;
-  border-radius: 20px;
-  margin: 0 0.5vw;
-}
-.badge-fire {
-  background-color: #f57d31;
-}
-
-.badge-water {
-  background-color: #6493eb;
-}
-
-.badge-grass {
-  background-color: #75cb48;
-}
-.badge-electric {
-  background-color: #f9cf30;
-}
-.badge-psychic {
-  background-color: #fb5584;
-}
-.badge-darkness {
-  background-color: #75574c;
-}
-.badge-fighting {
-  background-color: #c12239;
-}
-.badge-metal {
-  background-color: #b7b9d0;
-}
-.badge-dragon {
-  background-color: #7037ff;
-}
-.badge-fairy {
-  background-color: #e69eac;
-}
-.badge-colorless {
-  background-color: #aaa67f;
-}
-.badge-default {
-  background-color: #d2d1c1;
-}
-.badge-variant {
-  padding: 0.5vh 0.5vh;
-  margin: 0;
-  background-color: white;
-  border-radius: 15px;
-  border: 1px solid transparent;
-  width: calc(100% - 1px);
-}
-.badge-normal {
-  border: 2px solid #8d774f;
-  background-color: transparent;
-}
-
-.badge-reverse {
-  border: 2px solid #332f2f;
-  background-color: transparent;
-}
-.badge-firstEdition {
-  border: 2px solid #6493eb;
-  background-color: transparent;
-}
-.badge-holo {
-  padding: 0.5vh 0vw;
-  border: 3px solid transparent;
-  border-radius: 20px;
-  margin: 0 0.5vw;
-  background: rgb(255, 153, 69);
-  background: linear-gradient(
-    90deg,
-    rgba(255, 69, 69, 1) 0%,
-    rgba(251, 163, 72, 1) 12%,
-    rgba(242, 244, 81, 1) 23%,
-    rgba(181, 255, 84, 1) 33%,
-    rgba(118, 243, 186, 1) 46%,
-    rgba(99, 184, 248, 1) 56%,
-    rgba(153, 150, 249, 1) 70%,
-    rgba(252, 169, 248, 1) 84%,
-    rgba(255, 125, 152, 1) 93%,
-    rgba(255, 69, 69, 1) 100%
-  );
-}
-.badge-wPromo {
-  border: 2px solid #fb5584;
-  background-color: transparent;
 }
 
 .btn-close {
@@ -390,5 +261,64 @@ export default {
   background: #4aae9b;
   border: 1px solid #4aae9b;
   border-radius: 2px;
+}
+
+.badge-default {
+  padding: 0.5vh 0.5vh;
+  background-color: #d2d1c1;
+  border: 3px solid transparent;
+  border-radius: 20px;
+  margin: 0 0.5vw;
+}
+
+.badge-child-holo {
+  padding: 0.5vh 0.5vh;
+  margin: 0;
+  background-color: white;
+  border-radius: 15px;
+  border: 1px solid transparent;
+  width: calc(100% - 1px);
+}
+
+.evolution-pokemon {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  padding: 1vh 0;
+}
+.child-pokemon-img,
+.pokemon-initial-img {
+  border-radius: 360px;
+  border: 3px solid #d2d1c1;
+  background: rgb(249, 210, 150);
+  background: radial-gradient(circle, #aaa67f, rgba(255, 255, 255, 0) 52%);
+}
+@media (max-width: 768px) {
+  .modal-body {
+    flex-direction: column;
+    padding: 20px 10px; /* Ajuste l'espacement si nécessaire */
+  }
+
+  .left-section,
+  .right-section {
+    border: none; /* Supprime la bordure sur les petits écrans */
+    padding: 0;
+    margin: 0;
+  }
+
+  .left-section {
+    display: flex;
+    justify-content: center; /* Centrer l'image sur petits écrans */
+    padding-bottom: 10px;
+  }
+
+  .right-section {
+    margin-top: 20px; /* Ajoute un espace entre les sections */
+  }
+
+  .left-section img {
+    max-height: 100%;
+    height: 60vh;
+  }
 }
 </style>
